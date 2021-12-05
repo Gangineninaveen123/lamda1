bd6f8580a9faf78b56a22755c0e4f005ecad3bfa
G Naveen <93296382+Gangineninaveen123@users.noreply.github.com>
2021-12-05 08:25:25 +0530

    Add files via upload
    triplet of list

---
+++ Write a Python program to triple all numbers of a given list of integers. Use Python map..py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+sample_list  = [1,2,3,4,5,6,7]
+print('sample list:',sample_list)
+function = map(lambda i : i*3 , sample_list)
+print('\n Triple of list numbers:')
+print(list(function))
+
+
+# In[ ]:
+
+
+
+
